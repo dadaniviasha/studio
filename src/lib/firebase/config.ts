b3df8a58f5a0a9d2b6bccb99332fbaa41f34e750
Vipasha Dadani <dadaniviasha@gmail.com>
2025-06-21 07:07:50 +0000

    make some changes and restart

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,3 +1,4 @@
+// Restarting server to load new environment variables.
 
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
